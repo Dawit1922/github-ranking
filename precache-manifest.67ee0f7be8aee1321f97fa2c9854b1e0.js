48e1ec1ff73261cb001a1a79720bb877c11f6058
Dawit1922 <davidkiros11@gmail.com>
2020-09-08 12:39:28 +0530

    Updates

---
+++ precache-manifest.67ee0f7be8aee1321f97fa2c9854b1e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258f93d72192a4ead88389493960953d",
+    "url": "/github-ranking/index.html"
+  },
+  {
+    "revision": "9f8adf15f87e1b3b4e75",
+    "url": "/github-ranking/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3d2b1e4fe818b7280e6",
+    "url": "/github-ranking/static/js/2.72bd8633.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-ranking/static/js/2.72bd8633.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f8adf15f87e1b3b4e75",
+    "url": "/github-ranking/static/js/main.7ccf55a6.chunk.js"
+  },
+  {
+    "revision": "280fe26fb0df46a06828",
+    "url": "/github-ranking/static/js/runtime-main.64bfbd1d.js"
+  }
+]);
